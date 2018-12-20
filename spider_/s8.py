388d69b098d7082d4dae10b10f4d6734958d4a73
disen <610039018@qq.com>
2018-12-20 14:38:03 +0800

    人人网登录接口与参数

---
+++ spider_/s8.py
@@ -0,0 +1,19 @@
+
+url = 'http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=20181141452439'
+
+params = {
+    'email': '',
+    'icode': '',
+    'origURL': 'http://www.renren.com/',
+    'domail': 'www.renren.com',
+    'key_id': 1,
+    'captcha_type': 'web_login',
+    'password': '8268f673a8680c25fc8f40b50e6bc0b28a5fdb8b721f89549478be191249fd6a',
+    'rkey': '39b392090c635431e86ef76d46f31f40',
+    'f':''
+}
+
+headers = {
+    'User-Agent': 'QF-XA-PY05-Spider'
+}
+
