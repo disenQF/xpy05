475667636739aaa996af54016dfbcd38a3a2a593
disen <610039018@qq.com>
2018-12-20 15:06:03 +0800

    CookieJar的应用

--- spider_/s8.py
+++ spider_/s8.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlencode
+from urllib.request import HTTPCookieProcessor, Request, build_opener
+from http.cookiejar import CookieJar
 
 url = 'http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=20181141452439'
 
@@ -8,12 +11,47 @@ params = {
     'domail': 'www.renren.com',
     'key_id': 1,
     'captcha_type': 'web_login',
-    'password': '8268f673a8680c25fc8f40b50e6bc0b28a5fdb8b721f89549478be191249fd6a',
+    'password': '',
     'rkey': '39b392090c635431e86ef76d46f31f40',
-    'f':''
+    'f': ''
 }
 
 headers = {
     'User-Agent': 'QF-XA-PY05-Spider'
 }
 
+
+def build_browser():  # [fn] + shift + F6 重构的重命名
+    # 1. 创建Handler-> HTTPCookieProcessor(CookieJar)
+    handler = HTTPCookieProcessor(CookieJar())  # 会保留Cookie的信息
+
+    # 2. 创建opener
+    return build_opener(handler)
+
+
+def login(opener):
+    req = Request(url, urlencode(params).encode(), headers)
+    resp = opener.open(req)
+    resp_txt = resp.read()
+    print(resp_txt.decode())
+
+
+def home(opener):
+    #  登录成功后，再次请求时，会使用CookieJar中保存的cookie数据
+    resp = opener.open('http://www.renren.com/top')
+    with open('renren-index.html', 'wb') as f:
+        f.write(resp.read())
+
+    resp = opener.open('http://www.renren.com/349584673/profile')
+    with open('renren-profile.html', 'wb') as f:
+        f.write(resp.read())
+
+    resp = opener.open('http://friend.renren.com/managefriends')
+    with open('renren-friends.html', 'wb') as f:
+        f.write(resp.read())
+
+
+if __name__ == '__main__':
+    browser = build_browser()
+    login(browser)
+    home(browser)
